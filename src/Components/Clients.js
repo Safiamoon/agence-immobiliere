2fc5578f787e97da23e374d99c541f9e6b782dac
Safiamoon <msellek.safia@gmail.com>
2020-11-12 17:47:37 +0100

    added build

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -17,7 +17,6 @@ class Appartements extends React.Component {
                     birthDate : "",
                     nationality : "",
                     bookings : []
-                
             },
 
             addClient: {
