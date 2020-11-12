5a5095c4b60033049589ae0cb266539893ecff38
Safiamoon <msellek.safia@gmail.com>
2020-11-12 21:32:18 +0100

    added netlify.toml

--- src/Components/Appartements.js
+++ src/Components/Appartements.js
@@ -73,7 +73,7 @@ class Appartements extends React.Component {
         axios.post("https://app-booking-christ.herokuapp.com/api/room",this.state.apartmentRoom)
         .then( (response) => {
             console.log(response.data);
-            this.props.history.push('/appartements') 
+            window.location.reload(false);  
         }).catch((error)=>{
             console.log(error)
         });
@@ -107,7 +107,7 @@ class Appartements extends React.Component {
         axios.post("https://app-booking-christ.herokuapp.com/api/apartment",this.state.AddApartment)
         .then( (response) => {
             console.log(response.data);
-            this.props.history.push('/appartements')   
+            window.location.reload(false);  
         }).catch((error)=>{
             console.log(error)
         });
